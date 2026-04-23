fadf07fc72e907e9f01f7a50244a6bd5ddfde882
Christine McKay <mckay6@wisc.edu>
2026-04-23 15:21:07 -0500

    puppeteer

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // This is for all pages
+// import puppetteer
+
+const puppeteer = require("puppeteer");
+
+async function go() {
+  // launch the browser
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+}
